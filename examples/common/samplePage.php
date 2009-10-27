05658052eee9f1806cbf691113a57086a670beea
Steve Loeppky <stvn@loeppky.com>
2009-10-27 16:37:41 -0600

    Added an example that demonstrates the problem with Ext's FileUploadButton.  This can be viewed at: http://loeppky.com/steven/code/ExtJsExtensions/examples/FileUploadField/?showBrowseButton=0&useExtFileUploadField=0

--- examples/common/samplePage.php
+++ examples/common/samplePage.php
@@ -2,6 +2,7 @@
 	$title = "Sample Page";
 	require_once("htmlToTitle.php");
 	require_once("extJsIncludes.php");
+	require_once("iconIncludes.php");
 	require_once("endHeadToBody.php");
 	require_once("footer.php");
 ?>
